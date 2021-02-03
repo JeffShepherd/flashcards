2468eb33d6291202bc60d8ba248ff1ebfb6e5604
JS <jeff.shep777@gmail.com>
2021-02-03 14:59:26 -0700

    Run linter and correct relevant errors

--- src/Round.js
+++ src/Round.js
@@ -31,6 +31,6 @@ class Round {
     console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`);
   }
 
-};
+}
 
 module.exports = Round;
--- src/Turn.js
+++ src/Turn.js
@@ -3,29 +3,29 @@ class Turn {
   constructor(guess, currentCard) {
     this.guess = guess;
     this.currentCard = currentCard;
-  };
+  }
   
   returnGuess() {
     return this.guess;
-  };
+  }
 
   returnCard() {
     return this.currentCard;
-  };
+  }
 
   evaluateCard() {
     return this.guess === this.currentCard.correctAnswer; 
   }
 
   giveFeedback() {
-    if(this.evaluateCard()) {
+    if (this.evaluateCard()) {
       return 'correct!';
     } else {
       return 'incorrect!'
     }
-  };
+  }
 
-};
+}
 
 
 module.exports = Turn;
