151694e9d22dde09c8f34fa8a65ef9703efb229d
JS <jeff.shep777@gmail.com>
2021-02-03 07:56:03 -0700

    Add takeTurn method and turn increment functionality within

--- src/Round.js
+++ src/Round.js
@@ -2,11 +2,22 @@ class Round {
   constructor(deck) {
     this.deck = deck;
     this.currentCard;
+    this.turns = 0;
+    this.incorrectGuesses = [];
   }
 
   returnCurrentCard() {
     this.currentCard = this.deck.cards[0];
     return this.currentCard;
+  }
+
+  takeTurn(guess) {
+     this.turns++
+    // let turn = new Turn(guess, this.returnCurrentCard())
+
+    //if (!turn.evaluateGuess()) {
+
+    // }
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -26,6 +26,23 @@ describe('Round', function() {
     expect(round.returnCurrentCard()).to.equal(card1);
   });
 
+  describe('takeTurn', function() {
+
+    it('should update turn count', function() {
+      const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+      const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+      const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+      const deck = new Deck([card1, card2, card3]);
+      const round = new Round(deck);
+  
+      round.takeTurn('William');
+      expect(round.turns).to.equal(1);
+      round.takeTurn('William');
+      expect(round.turns).to.equal(2);
+    });
+
+
+  });
 
 
 
