dddf59402d69784c61419dd9ad9adb47195797d9
JS <jeff.shep777@gmail.com>
2021-02-03 16:56:14 -0700

    Add functionality to Game start method

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,26 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('./Card');
+const Deck = require('./Deck');
+const Round = require('./Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound;
+
+  }
+
+  start() {
+    const cards = [];
+
+    prototypeQuestions.map(dataObject => {
+      cards.push(new Card(dataObject.id, dataObject.question, dataObject.answers, dataObject.correctAnswer));
+    });
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
--- test/Game-test.js
+++ test/Game-test.js
@@ -17,15 +17,23 @@ describe('Game', function() {
     expect(game).to.be.an.instanceof(Game);
   }); 
 
-  // it('should return the current card being played', function() {
-  //   const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-  //   const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-  //   const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-  //   const deck = new Deck([card1, card2, card3]);
-  //   const round = new Round(deck);
+  it('should have a property of currentRound default to undefined', function() {
+    const game = new Game();
+    expect(game.currentRound).to.equal(undefined);
+  }); 
 
-  //   expect(round.returnCurrentCard()).to.equal(card1);
-  // });
+  describe('start()', function() {
+    // it('should return the current card being played', function() {
+    //   const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    //   const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    //   const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    //   const deck = new Deck([card1, card2, card3]);
+    //   const round = new Round(deck);
+
+    // //   expect(round.returnCurrentCard()).to.equal(card1);
+    // });
+  });
+
 
 
 });
