fdcb34a71fb61da5bb1ecd09f7d1ac51c3fab49a
JS <jeff.shep777@gmail.com>
2021-02-02 19:26:53 -0700

    Add cards property, count method, and applicable tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
+const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const expect = chai.expect;
 
@@ -14,50 +15,21 @@ describe('Deck', function() {
     expect(deck).to.be.an.instanceof(Deck);
   }); 
 
-  // it('should store a user\'s guess to the question', function() {
-  //   const turn = new Turn('callback function');
-  //   expect(turn.guess).to.equal('callback function');
-  // });  
+  it('should be instantiated with an array of cards', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const deck = new Deck([card1, card2]);
 
-  // it('should store the current card in play', function() {
-  //   const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-  //   const turn = new Turn('object literal', card);
+    expect(deck.cards).to.deep.equal([card1, card2]);
+  });  
 
-  //   expect(turn.currentCard).to.be.an.instanceof(Card);
-  //   expect(turn.currentCard.answers).to.deep.equal(['object', 'array', 'function']);
-  // });  
+  it('should know how many cards are in the deck', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
 
-  // it('should return the guess', function() {
-  //   const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-  //   const turn = new Turn('object literal', card);
-
-  //   expect(turn.returnGuess()).to.equal('object literal');
-  // });
-
-  // it('should return the current card', function() {
-  //   const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-  //   const turn = new Turn('string literal', card);
-
-  //   expect(turn.returnCard()).to.be.an.instanceof(Card);
-  //   expect(turn.returnCard()).to.equal(card);
-  // });
-
-  // it('should evalauate guess vs correct answer and return boolean value', function() {
-  //   const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-  //   const turn1 = new Turn('object', card);
-  //   const turn2 = new Turn('string literal', card);
-
-  //   expect(turn1.evaluateCard()).to.equal(true);
-  //   expect(turn2.evaluateCard()).to.equal(false);
-  // });
-
-  // it('should return incorrect or correct based on accuracy of guess', function() {
-  //   const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-  //   const turn1 = new Turn('object', card);
-  //   const turn2 = new Turn('string literal', card);
-
-  //   expect(turn1.giveFeedback()).to.equal('correct!');
-  //   expect(turn2.giveFeedback()).to.equal('incorrect!');
-  // });
+    expect(deck.countCards()).to.equal(3);
+  });
 
 });
